3dafd3c74651eec2a7841131311c093238646d1d
DilshodIskandarov456 <76392416+DilshodIskandarov456@users.noreply.github.com>
2021-01-11 21:58:15 +0300

    news

--- app/src/main/java/com/example/youtube/MainActivity.java
+++ app/src/main/java/com/example/youtube/MainActivity.java
@@ -10,30 +10,24 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     public WebView myWebView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         myWebView = (WebView)findViewById(R.id.web_view);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("https://www.youtube.com/");
         // Code for opening the app
-
         myWebView.getSettings().setBuiltInZoomControls(true);
         myWebView.getSettings().setUseWideViewPort(true);
         myWebView.setWebViewClient(new WebViewClient());
-
     }
     // Code for back button
     public void onBackPressed(){
         if (myWebView.canGoBack())
         {
             myWebView.goBack();
-
         }else {
             super.onBackPressed();
         }
